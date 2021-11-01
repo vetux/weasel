123d7382973386c66177e3a988ea57d29d07ca73
xenotux <xenotux@outlook.com>
2021-11-01 13:34:11 +0100

    Set required cxx standard

--- cmake/system.cmake
+++ cmake/system.cmake
@@ -1,5 +1,8 @@
 file(GLOB_RECURSE System.Source src/system/*.cpp)
 
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
 add_library(weasel STATIC SHARED ${System.Source})
 
 target_include_directories(weasel PUBLIC include/)
